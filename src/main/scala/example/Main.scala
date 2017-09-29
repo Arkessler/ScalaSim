cc5ccae0f0f251be2af76398dd18f61b0a35f122
Davis Liu <davisliu@daviss-mbp.internal.bwater.com>
2017-09-29 14:18:15 -0400

    Add plant seeding

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -33,6 +33,7 @@ object Main extends App {
 	// Probably make some of these non-global at some point
 	var r = scala.util.Random
 	val maxPlantSize = 5
+	val maxPlants = 30
 
 	def clearScreen = print("\u001b[2J")
 
@@ -46,6 +47,10 @@ object Main extends App {
 		println()
 	}
 
+	// Note: our gmap implementation is weird, we probably should've just gone with 3
+	// lists of the stuff and built the map up from that; currently we had to do some
+	// weird stuff to avoid  issues of an herbivore wanting to move into the same spot 
+	// that a new plant is being placed or a carnivore wants to move
 	def update(gmap: Map): Map = {
 		val (w, h) = (gmap.length, gmap(0).length)
 
@@ -56,19 +61,20 @@ object Main extends App {
 		}
 
 		// Get adjacent locations that satisfy the valid parameter
-		def getValidAdj(currLoc: (Int,Int), valid: (Int,Int) => Boolean) = {
+		def getConditionalAdj(currLoc: (Int,Int), cond: (Int,Int) => Boolean) = {
 			val (x, y) = currLoc
 			val locs = List( (x+1, y), (x-1, y), (x, y+1), (x, y-1) )
-			locs.filter { case (x,y) => valid(x,y) }
+			locs.filter { case (x,y) => cond(x,y) }
 		}
 
-		val oldPlants : List[Plant] = gmap.flatten.filter(_.isPlant).map(_.asInstanceOf[Plant]).toList
-		val oldHerbs : List[Animal] = gmap.flatten.filter(_.isAnimal).map(_.asInstanceOf[Animal]).toList
+		val oldPlants = gmap.flatten.collect{ case p @ Plant(_,_,_,_) => p }.toList
+		val oldHerbs = gmap.flatten.collect{ case h @ Animal("herbivore",_,_) => h }.toList
+		val oldCarns = gmap.flatten.collect{ case c @ Animal("carnivore",_,_) => c }.toList
 
-		// Plants currently get eaten in a circle by herbivores
-		val newPlants = 
+		// Plants currently get eaten "in a circle" by herbivores
+		val updatedPlants = 
 			oldPlants.map((p: Plant) => {
-				val adjHerbs = getValidAdj(p.loc, (i:Int, j:Int) => {
+				val adjHerbs = getConditionalAdj(p.loc, (i:Int, j:Int) => {
 					checkBounds(i,j) && gmap(i)(j).species == "herbivore"
 				})
 				
@@ -78,32 +84,54 @@ object Main extends App {
 						else p.size
 					else p.size - adjHerbs.length
 
-				// To Implement: Randomly generate new plants
-
+				// Decide whether to be lower or uppercase
 				if (newSize <= maxPlantSize/2)
 					p.copy(size = newSize, sym = p.sym.toLower)
 				else 
 					p.copy(size = newSize, sym = p.sym.toUpper)
 			}).filter(_.size > 0)
 
+		val newPlants = 
+			updatedPlants.foldLeft(List.empty : List[Plant])((newPs: List[Plant], p: Plant) => {
+				// Decide whether or not to generate a plant
+				val totalPlants = oldPlants.length + newPs.length
+				val genChance = if (totalPlants == 0) 1 
+				                else 1 - totalPlants.toDouble/maxPlants.toDouble
+				if (r.nextFloat < genChance) {
+					// Possible adj spots for new plants
+					val plantLocs = getConditionalAdj(p.loc, (i:Int, j:Int) => {
+						val newLocs = newPs.map(_.loc)
+						checkBounds(i,j) && gmap(i)(j).isTerrain && !newLocs.contains((i,j))
+					})
+					if (!plantLocs.isEmpty) {
+						val rand = r.nextInt(plantLocs.length)
+						val newP = Plant("bush", 'o', 1, plantLocs(rand))
+						newP :: newPs
+					}
+					else newPs
+				}
+				else newPs
+			}) ::: updatedPlants
+
 		val newHerbs =
-			oldHerbs.foldLeft(List.empty : List[Animal])((newHerbs : List[Animal], h : Animal) => {
+			oldHerbs.foldLeft(List.empty : List[Animal])((newHs: List[Animal], h: Animal) => {
 				// Stay next to adjacent plants
-				val plantLocs = getValidAdj(h.loc, (i:Int, j:Int) =>
+				val plantLocs = getConditionalAdj(h.loc, (i:Int, j:Int) =>
 					 checkBounds(i,j) && gmap(i)(j).isPlant
 				)
-				if (!plantLocs.isEmpty) h :: newHerbs else {
+				if (!plantLocs.isEmpty) h :: newHs else {
 					// Randomly move to an empty adjacent location
-					val validLocs = getValidAdj(h.loc, (i:Int, j:Int) => {
-						val newLocs = newHerbs.map(_.loc)
-						checkBounds(i,j) && gmap(i)(j).isTerrain && !newLocs.contains((i,j))
+					val validLocs = getConditionalAdj(h.loc, (i:Int, j:Int) => {
+						val newLocs = newHs.map(_.loc)
+						checkBounds(i,j) && gmap(i)(j).isTerrain &&
+							!newLocs.contains((i,j)) && !newPlants.contains((i,j))
 					})
 
 					if (!validLocs.isEmpty) {
 						val rand = r.nextInt(validLocs.length)
-						h.copy(loc = validLocs(rand)) :: newHerbs
+						h.copy(loc = validLocs(rand)) :: newHs
 					}
-					else h :: newHerbs
+					else h :: newHs
 				}
 			})
 
@@ -111,7 +139,7 @@ object Main extends App {
 			val dirt = Terrain("dirt", '.', (x, y))
 			if (newHerbs.map(_.loc).contains((x, y)))
 				newHerbs.find(_.loc == (x,y)).getOrElse(dirt)
-			else if (oldPlants.map(_.loc).contains((x,y))) 
+			else if (newPlants.map(_.loc).contains((x,y))) 
 				newPlants.find(_.loc == (x,y)).getOrElse(dirt)
 			else 
 				dirt
@@ -124,6 +152,7 @@ object Main extends App {
 		val initW = 30
 		val initH = 15
 		val plantChance = .05
+		val carnivoreLocations = List((1,1), (12,12), (8,18))
 		val herbivoreLocations = List((5,10), (25,5), (3,3), (4,4), (5,5), (20, 8))
 
 		assert(initW > 0)
@@ -131,6 +160,8 @@ object Main extends App {
 		Array.tabulate(initW, initH)((x, y) =>
 			if (herbivoreLocations.contains((x, y)))
 				Animal("herbivore", 'H', (x, y))
+			else if (carnivoreLocations.contains(x,y))
+				Animal("carnivore", 'C', (x,y))
 			else if (r.nextFloat < plantChance) 
 				// randomize initial size
 				Plant("bush", 'O', maxPlantSize, (x, y)) 
